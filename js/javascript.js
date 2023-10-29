888a01586e401c3c1c978d2c1ec72d3b0f59ebc6
Mariana García Dolz <marianadolz.mgd@gmail.com>
2023-10-29 20:12:55 -0300

    agregue formulario de consulta
    agregue formulario de consulta

---
+++ js/javascript.js
@@ -0,0 +1,49 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+    }, false)
+    })
+})()
+
+function validarDatos() {
+    if(formConsultas.nombre.value ==0){
+        alert("Complete el nombre mínimo 3 caracteres")
+        formConsultas.nombre.value ="";
+        formConsultas.nombre.focus();
+        return false;
+    }
+    else  if(formConsultas.apellido.value ==0){
+    alert("Complete el apellido mínimo 3 caracteres")
+    formConsultas.apellido.value ="";
+    formConsultas.apellido.focus();
+    return false;
+    }
+    else  if(formConsultas.email.value ==0){
+    alert("Complete el mail mínimo 3 caracteres")
+    formConsultas.email.value ="";
+    formConsultas.email.focus();
+    return false;
+    }
+    else  if(formConsultas.consulta.value ==0){
+        alert("Complete la cosulta mínimo 5 palabras")
+        formConsultas.consulta.value ="";
+        formConsultas.consulta.focus();
+        return false;
+    }
+    else{
+        alert("Felicitaciones hemos recibido tu consulta");
+    }
+}
